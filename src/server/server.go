d040c1fd8d143e08ff32388d12c42d0ca9b22f9c
boboman13 <rich@safe-node.com>
2014-01-28 00:01:50 -0800

    Stop it with the derps @boboman13

--- src/server/server.go
+++ src/server/server.go
@@ -23,10 +23,22 @@ func UploadHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "<html><head><title>Upload Page</title></head><body>Dat Upload Page</body></html>")
 }
 
+// The site.css file.
+func StyleHandler(writer http.ResponseWriter, request *http.Request) {
+	content, err := ioutil.ReadFile("static/site.css")
+	if err != nil {
+		fmt.Println("Error occurred while loading static/site.css: " + err.Error())
+		return
+	}
+
+	writer.Write(content)
+}
+
 // Starts the UpImgServer.
 func Start(port string) {
 	http.HandleFunc("/", IndexHandler)
 	http.HandleFunc("/upload", UploadHandler)
+	http.HandleFunc("/site.css", StyleHandler)
 
 	// Listen and serve.
 	http.ListenAndServe(port, nil)
