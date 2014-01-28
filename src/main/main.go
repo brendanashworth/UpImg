b0ea516ee10a1e174f132b18459f22c7dc440e6d
boboman13 <rich@safe-node.com>
2014-01-28 09:59:34 -0800

    Limit by Go max processors

--- src/main/main.go
+++ src/main/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"server"
+	"runtime"
 )
 
 func main() {
+	// Setup Go stuff
+	rumtime.GOMAXPROCS(runtime.NumCPU())
+
 	fmt.Println("Starting UpImg server on :8080")
 	
 	// Create the server and start it.
