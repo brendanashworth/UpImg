ad447a4667947e7bd4cd5207a166dc1a82732577
boboman13 <rich@safe-node.com>
2014-01-27 23:27:47 -0800

    Fixes another compiling error

--- src/server/server.go
+++ src/server/server.go
@@ -10,19 +10,19 @@ type UpImgServer struct {
 }
 
 // The index page. This also handles 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "<html><head><title>UpImg</title></head><body><h1>UPIMG UPLOAD UR FILEZ</body></html>")
 }
 
 // The upload page.
-func UploadHandler(w http.ResponseWriter, r *http.Request) {
+func UploadHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "<html><head><title>uploadz</title></head><body>dez uploads</body></html>")
 }
 
 // Starts the UpImgServer.
 func (*UpImgServer) Start(port string) (output string) {
-	http.handleFunc("/", IndexHandler)
-	http.handleFunc("/upload", UploadHandler)
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/upload", UploadHandler)
 
 	// Listen and serve.
 	http.ListenAndServe(port, nil)
